09ce65eada4b1d389ff1dae2d62348dc1a4c978f
lpotvin <ludopotvin@gmail.com>
2025-11-05 10:27:52 -0500

    add db creation

--- src/passmanager/db.py
+++ src/passmanager/db.py
@@ -8,3 +8,7 @@ def init_db() -> None:
     path: Path = Path(db_path)
 
     absolute_path = Path(__file__).resolve().parent.joinpath(db_path).resolve()
+
+    absolute_path.parent.mkdir(parents=True, exist_ok=True)
+    conn = sqlite3.connect(str(absolute_path))
+    conn.close()
