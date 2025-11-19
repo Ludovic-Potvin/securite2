ac1f93670ba4743c3dce965e3c2ab52fd5b630a0
lpotvin <ludopotvin@gmail.com>
2025-11-18 19:59:32 -0500

    sqlcipher added

--- src/passmanager/database.py
+++ src/passmanager/database.py
@@ -1,15 +1,17 @@
 import os
 from pathlib import Path
 from peewee import SqliteDatabase, Model
+from playhouse.sqlcipher_ext import SqlCipherDatabase
 
 
 DB_LOCATION: str = os.getenv(
     "DB_PATH", str(Path(__file__).resolve().parent.parent.parent / "db" / "database.db")
 )
+DB_PASSWORD: str = os.getenv("DB_PASSWORD", "SuperSecretPassword123")
 
 Path(DB_LOCATION).parent.mkdir(parents=True, exist_ok=True)
 
-db: SqliteDatabase = SqliteDatabase(database=DB_LOCATION)
+db: SqliteDatabase = SqlCipherDatabase(database=DB_LOCATION, passphrase=DB_PASSWORD)
 
 
 class BaseModel(Model):
